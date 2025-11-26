5bf8b46f15e8922bd74bb85081bbed25c4bc5abe
A-Rush <83367414+Ara-13@users.noreply.github.com>
2025-11-26 11:33:26 +0330

    Create d2b.c
    تبدیل یک عدد دسیمال مثبت به معادل باینری اش

---
+++ d2b.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// آرش دوالی
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    if(n==1 || n==0){
+        printf("%d", n);
+    }else{
+        int i=1, result = 0;
+        while( n >= 1){
+            result += (n%2)*i;
+            i = i*10;
+            n = (int)(n/2);
+        }
+        printf("%d", result);
+    }
+}
